0e74d0315a8feb60215af06e8cf9c17618ca6bd7
Plum-afk <24103848@usc.edu.ph>
2026-03-12 01:56:40 +0800

    Add email validation

--- solo-todo-list/Views/SignUpPage.xaml.cs
+++ solo-todo-list/Views/SignUpPage.xaml.cs
@@ -12,7 +12,7 @@ public partial class SignUpPage : ContentPage
 
     private async void SignUpBtn_Clicked(object sender, EventArgs e)
     {
-        if (NameEntry.Text != null && EmailEntry.Text != null && PasswordEntry.Text != null && ConfirmPasswordEntry.Text != null && PasswordEntry.Text == ConfirmPasswordEntry.Text)
+        if (NameEntry.Text != null && EmailEntry.Text != null && EmailEntry.Text.Contains("@") && PasswordEntry.Text != null && ConfirmPasswordEntry.Text != null && PasswordEntry.Text == ConfirmPasswordEntry.Text)
         {
             UserRepository.SignUpUser(NameEntry.Text, EmailEntry.Text, PasswordEntry.Text);
             await Navigation.PopAsync();
