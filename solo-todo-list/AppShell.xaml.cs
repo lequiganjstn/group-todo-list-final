2c0a940e8f35e5572dafd33d492e673a10db93ac
Plum-afk <24103848@usc.edu.ph>
2026-03-12 01:20:06 +0800

    Add routes

--- solo-todo-list/AppShell.xaml.cs
+++ solo-todo-list/AppShell.xaml.cs
@@ -7,6 +7,15 @@ namespace solo_todo_list
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute(nameof(AddTaskPage), typeof(AddTaskPage));
+            Routing.RegisterRoute(nameof(CompletedPage), typeof(CompletedPage));
+            Routing.RegisterRoute(nameof(EditCompletedTaskPage), typeof(EditTaskPage));
+            Routing.RegisterRoute(nameof(EditTaskPage), typeof(EditTaskPage));
+            Routing.RegisterRoute(nameof(ProfilePage), typeof(ProfilePage));
+            //Routing.RegisterRoute(nameof(SignInPage), typeof(SignInPage));
+            //Routing.RegisterRoute(nameof(SignUpPage), typeof(SignUpPage));
+            Routing.RegisterRoute(nameof(ToDoPage), typeof(ToDoPage));
         }
     }
 }
